3e945861d30b6d8d04a2d86f42a56ad9ca1dc8e2
Diana Mayhugh <dianamayhugh@gmail.com>
2020-08-07 13:18:57 -0400

    update data

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ var PORT = process.env.PORT || 3000;
 // Initialize Express
 var app = express();
 
-// Configure middlewares
+// Configure middleware
 
 // Use morgan logger for logging requests
 app.use(logger("dev"));
